cdc7fecd3b1f9777c5eb4d3db7228ea38bf546e1
Qs-F <qsf@mail.queensya.com>
2016-10-23 00:47:31 +0900

    Fix touch device bug
    Add e.preventDefault to avoid counting up by mouse event

--- longpress.js
+++ longpress.js
@@ -16,14 +16,15 @@ class LongPress {
       this.elem = d
       this.elem.addEventListener("touchstart", this.startTimer.bind(this), false)
       this.elem.addEventListener("mousedown", this.startTimer.bind(this), false)
-      this.elem.addEventListener("touchmove", this.clearTimer.bind(this), false)
+      this.elem.addEventListener("touchleave", this.clearTimer.bind(this), false)
       this.elem.addEventListener("mouseout", this.clearTimer.bind(this), false)
       this.elem.addEventListener("touchend", this.clearTimer.bind(this), false)
       this.elem.addEventListener("mouseup", this.clearTimer.bind(this), false)
       this.elem.addEventListener("touchcancel", this.clearTimer.bind(this), false)
   }
-  startTimer() {
+  startTimer(e) {
     (function() {
+      console.log("start")
       this.elem.classList.remove("pressed")
       this.elem.classList.add("pressing")
       this.timer = window.setTimeout(function(){
@@ -32,11 +33,12 @@ class LongPress {
         this.elem.classList.remove("pressing")
       }.bind(this), this.interval)
     }.bind(this))()
+    e.preventDefault()
   }
   clearTimer() {
     if(this.timer != undefined) {
+      this.elem.classList.remove("pressing")
       window.clearTimeout((function() {
-        this.elem.classList.remove("pressing")
         return this.timer
       }.bind(this))())
     }
