8c45f81f7db3595977e4c84a32dbc51142613c8f
Qs-F <qsf@mail.queensya.com>
2016-10-23 03:00:16 +0900

    Update longpress.js
    Update default interval time from 500 to 1000
    Update some indents
    Remove console.log for debug

--- longpress.js
+++ longpress.js
@@ -2,7 +2,7 @@
 
 class LongPress {
   constructor() {
-    this.interval = 500
+    this.interval = 1000
     this.callback = function(){}
   }
   SetInterval(s) {
@@ -12,19 +12,17 @@ class LongPress {
     this.callback = f
   }
   SetElement(d) {
-    var self = this
-      this.elem = d
-      this.elem.addEventListener("touchstart", this.startTimer.bind(this), false)
-      this.elem.addEventListener("mousedown", this.startTimer.bind(this), false)
-      this.elem.addEventListener("touchleave", this.clearTimer.bind(this), false)
-      this.elem.addEventListener("mouseout", this.clearTimer.bind(this), false)
-      this.elem.addEventListener("touchend", this.clearTimer.bind(this), false)
-      this.elem.addEventListener("mouseup", this.clearTimer.bind(this), false)
-      this.elem.addEventListener("touchcancel", this.clearTimer.bind(this), false)
+    this.elem = d
+    this.elem.addEventListener("touchstart", this.startTimer.bind(this), false)
+    this.elem.addEventListener("mousedown", this.startTimer.bind(this), false)
+    this.elem.addEventListener("touchleave", this.clearTimer.bind(this), false)
+    this.elem.addEventListener("mouseout", this.clearTimer.bind(this), false)
+    this.elem.addEventListener("touchend", this.clearTimer.bind(this), false)
+    this.elem.addEventListener("mouseup", this.clearTimer.bind(this), false)
+    this.elem.addEventListener("touchcancel", this.clearTimer.bind(this), false)
   }
   startTimer(e) {
     (function() {
-      console.log("start")
       this.elem.classList.remove("pressed")
       this.elem.classList.add("pressing")
       this.timer = window.setTimeout(function(){
